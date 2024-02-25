6bc8c322e5bee84c4268e9b0cc6df9df7ace0384
Abdul Waheed <meharwaheed4313@gmail.com>
2024-02-26 01:20:40 +0500

    comment added in consumerjs

--- client.js
+++ client.js
@@ -2,5 +2,5 @@ const { Kafka } = require("kafkajs");
 
 exports.kafka = new Kafka({
     clientId: "my-app",
-    brokers: ["192.168.100.17:9092"],
+    brokers: ["192.168.100.17:9092"], // ip of your network / Wi-Fi
 });
